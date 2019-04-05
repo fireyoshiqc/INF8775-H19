30f6b88bd4ce60e9a431fac04ade9790a18c434a
Fabrice Charbonneau <fabrice.charbonneau@hotmail.com>
2019-04-05 00:25:03 -0400

    genetic now fully functionnal, no crossover but surviving system, altering survivors to generate new population

--- TP3/inf8775tp3source/inf8775tp3source/genetic.cpp
+++ TP3/inf8775tp3source/inf8775tp3source/genetic.cpp
@@ -5,7 +5,6 @@ GeneticIndividual generateRandomIndividual(const Problem & problem) {
 	GeneticIndividual individual;
 	individual.budget = problem.availablePieceBudget;
 	individual.modelGenes = std::vector<int> (problem.nModels);
-	individual.fitness = 0.0;
 	bool areAllUsed = false;
 	while (!areAllUsed)
 	{
@@ -20,9 +19,9 @@ GeneticIndividual generateRandomIndividual(const Problem & problem) {
 		areAllUsed = std::all_of(individual.budget.begin(), individual.budget.end(), [](int amount) { return amount <= 0; });
 	}
 	
-	for (size_t i = 0; i < individual.modelGenes.size(); i++) {
-		individual.fitness += (float)(1 / (float) std::accumulate(begin(problem.models[i].modelCosts), end(problem.models[i].modelCosts), 0));
-	}
+
+	individual.totalCost = abs(std::accumulate(individual.budget.begin(), individual.budget.end(), 0));
+	
 
 	return individual;
 }
@@ -39,63 +38,147 @@ void switchIndividualGenes(GeneticIndividual & individual, const Problem & probl
 	individual.modelGenes[addModelIdx] ++;
 	individual.modelGenes[subModelIdx] --;
 
-	for (size_t i = 0; i < individual.budget.size(); i++)
+	// update budget
+	for (int i = 0; i < individual.budget.size(); i++)
 	{
 		individual.budget[i] -= problem.models[addModelIdx].modelCosts[i];
 	}
 
-	for (size_t i = 0; i < individual.budget.size(); i++)
+	for (int i = 0; i < individual.budget.size(); i++)
 	{
 		individual.budget[i] += problem.models[subModelIdx].modelCosts[i];
 	}
 
-
-	individual.fitness += (float)(1 / (float) std::accumulate(begin(problem.models[addModelIdx].modelCosts), end(problem.models[addModelIdx].modelCosts), 0));
-	individual.fitness -= (float)(1 / (float) std::accumulate(begin(problem.models[subModelIdx].modelCosts), end(problem.models[subModelIdx].modelCosts), 0));
+	//update cost (or "fitness")
+	individual.totalCost += std::accumulate(begin(problem.models[addModelIdx].modelCosts), end(problem.models[addModelIdx].modelCosts), 0);
+	individual.totalCost -= std::accumulate(begin(problem.models[subModelIdx].modelCosts), end(problem.models[subModelIdx].modelCosts), 0);
 
 }
 
 void addIndividualGenes(GeneticIndividual & individual, const Problem & problem, size_t index) {
 	individual.modelGenes[index] ++;
-	individual.fitness += (float)(1 / (float) std::accumulate(begin(problem.models[index].modelCosts), end(problem.models[index].modelCosts), 0));
+
+	// update budget
+	for (size_t i = 0; i < individual.budget.size(); i++)
+	{
+		individual.budget[i] -= problem.models[index].modelCosts[i];
+	}
+
+	//update cost
+	individual.totalCost += std::accumulate(begin(problem.models[index].modelCosts), end(problem.models[index].modelCosts), 0);
 }
 
 void mutateIndividual(GeneticIndividual & individual, const Problem & problem) {
 	size_t addModelIdx, subModelIdx;
+	GeneticIndividual newIndividual = individual;
 	// switch MINIMAL_MUTATION_ITERATIONS models 
-	for (size_t i = 0; i < MINIMAL_MUTATION_ITERATIONS; i++) 
+	for (int i = 0; i < MINIMAL_MUTATION_ITERATIONS; i++) 
 	{
 
 		//find valid random genes to modify
-		addModelIdx = rand() % individual.modelGenes.size();
+		addModelIdx = rand() % newIndividual.modelGenes.size();
 		do {
-			subModelIdx = rand() % individual.modelGenes.size();
-		} while (individual.modelGenes[subModelIdx] != 0);
+			subModelIdx = rand() % newIndividual.modelGenes.size();
+		} while (newIndividual.modelGenes[subModelIdx] == 0);
 
-		switchIndividualGenes(individual, problem, addModelIdx, subModelIdx);
+		switchIndividualGenes(newIndividual, problem, addModelIdx, subModelIdx);
 
+	}
+
+	if (newIndividual.totalCost > individual.totalCost) {
+		return; // If the new indivdual is already worst when it is not even valid yet, stop. 
 	}
 	
 	// add models until individual is valid
-	bool areAllUsed = std::all_of(individual.budget.begin(), individual.budget.end(), [](int amount) { return amount <= 0; });
+	bool areAllUsed = std::all_of(newIndividual.budget.begin(), newIndividual.budget.end(), [](int amount) { return amount <= 0; });
+	bool isCostGreater = false;
 	while (!areAllUsed)
 	{
-		addModelIdx = rand() % individual.modelGenes.size();
-		addIndividualGenes(individual, problem, addModelIdx);
-		areAllUsed = std::all_of(individual.budget.begin(), individual.budget.end(), [](int amount) { return amount <= 0; });
+		addModelIdx = rand() % newIndividual.modelGenes.size();
+		addIndividualGenes(newIndividual, problem, addModelIdx);
+		areAllUsed = std::all_of(newIndividual.budget.begin(), newIndividual.budget.end(), [](int amount) { return amount <= 0; });
+		if (newIndividual.totalCost > individual.totalCost) {
+			return; // If the new indivdual is already worst when it is not even valid yet, stop. 
+		}
+	}
+	individual = newIndividual;
+}
+
+void alterIndividual(GeneticIndividual & individual, const Problem & problem) {
+	size_t addModelIdx, subModelIdx;
+	GeneticIndividual newIndividual = individual;
+	// switch MINIMAL_MUTATION_ITERATIONS models 
+	for (int i = 0; i < MINIMAL_ATLER_ITERATIONS; i++)
+	{
+		//find valid random genes to modify
+		addModelIdx = rand() % newIndividual.modelGenes.size();
+		do {
+			subModelIdx = rand() % newIndividual.modelGenes.size();
+		} while (newIndividual.modelGenes[subModelIdx] == 0);
+
+		switchIndividualGenes(newIndividual, problem, addModelIdx, subModelIdx);
 	}
 
+	// add models until individual is valid
+	bool areAllUsed = std::all_of(newIndividual.budget.begin(), newIndividual.budget.end(), [](int amount) { return amount <= 0; });
+	bool isCostGreater = false;
+	while (!areAllUsed)
+	{
+		addModelIdx = rand() % newIndividual.modelGenes.size();
+		addIndividualGenes(newIndividual, problem, addModelIdx);
+		areAllUsed = std::all_of(newIndividual.budget.begin(), newIndividual.budget.end(), [](int amount) { return amount <= 0; });
+	}
+	individual = newIndividual;
+}
+
+bool compareByCost(const GeneticIndividual &a, const GeneticIndividual &b)
+{
+	return a.totalCost < b.totalCost;
+}
+
+void sortPopulation(GeneticPopulation & population) {
+	std::sort(population.individuals.begin(), population.individuals.end(), compareByCost);
+}
+
+std::vector<GeneticIndividual> selectSurvivors(GeneticPopulation & population) {
+	sortPopulation(population);
+	std::vector<GeneticIndividual> survivors(NUMBER_OF_SURVIVORS);
+	survivors = slice(population.individuals, 0, NUMBER_OF_SURVIVORS);
+	return survivors;
+}
+
+void genocide(GeneticPopulation & population, std::vector<GeneticIndividual> survivors, const Problem & problem) {
+	for (int i = NUMBER_OF_SURVIVORS; i < POPULATION_SIZE; i++) {
+		int parentIdx = rand() % NUMBER_OF_SURVIVORS;
+		GeneticIndividual newIndividual = survivors[parentIdx];
+		alterIndividual(newIndividual, problem);
+		population.individuals[i] = newIndividual;
+	}
+}
+
+void printIndividual(GeneticIndividual ind) {
+	std::cout << "Best individual cost: " << ind.totalCost << std::endl;
+	std::cout << "Best individual solution: ";
+	for (int i = 0; i < ind.modelGenes.size(); i++) {
+		std::cout << ind.modelGenes[i] << " ";
+	}
+	std::cout << std::endl;
 }
 
 void evolve(GeneticPopulation & population, const Problem & problem) {
-	for (size_t i = 0; i < POPULATION_SIZE; i++) {
+	for (int i = 0; i < POPULATION_SIZE; i++) {
 		mutateIndividual(population.individuals[i], problem);
 	}
+	std::vector<GeneticIndividual> survivors = selectSurvivors(population);
+
+	printIndividual(survivors[0]);
+	
+	genocide(population, survivors, problem);
 }
 
 void solveGenetic(const Problem & problem) {
 	GeneticPopulation population = generateRandomPopulation(problem);
-	for (size_t i = 0; i < EVOLVE_ITERATIONS; i++) {
+	for (int i = 0; i < EVOLVE_ITERATIONS; i++) {
 		std::cout << "starting evolution #" << i << std::endl;
 		evolve(population, problem);
 		std::cout << "finished evolution #" << i << std::endl;
--- TP3/inf8775tp3source/inf8775tp3source/genetic.h
+++ TP3/inf8775tp3source/inf8775tp3source/genetic.h
@@ -3,15 +3,26 @@
 #include "problem.h"
 #include "solution.h"
 
-const size_t POPULATION_SIZE = 1;
-const size_t MINIMAL_MUTATION_ITERATIONS = 1;
-const size_t EVOLVE_ITERATIONS = 10;
+const int POPULATION_SIZE = 100;
+const int MINIMAL_MUTATION_ITERATIONS = 3;
+const int MINIMAL_ATLER_ITERATIONS = 5;
+const int EVOLVE_ITERATIONS = 1000;
+const int NUMBER_OF_SURVIVORS = 5; // Sandwiched by: 0 < NUMBER_OF_SURVIVOR <= POPULATION_SIZE
 
 struct GeneticIndividual {
 	std::vector<int> modelGenes;
 	std::vector<int> budget;
-	float fitness;
+	int totalCost;
 };
+
+// code taken from https://www.techiedelight.com/get-slice-sub-vector-from-vector-cpp/
+template<typename T>
+std::vector<T> slice(std::vector<T> &v, int m, int n)
+{
+	std::vector<T> vec(n - m + 1);
+	std::copy(v.begin() + m, v.begin() + n + 1, vec.begin());
+	return vec;
+}
 
 struct GeneticPopulation {
 	std::vector<GeneticIndividual> individuals;
@@ -27,6 +38,14 @@ void switchIndividualGenes(GeneticIndividual & individual, const Problem & probl
 
 void addIndividualGenes(GeneticIndividual & individual, const Problem & problem, size_t index);
 
+void sortPopulation(GeneticPopulation & population);
 
+std::vector<GeneticIndividual> selectSurvivors(GeneticPopulation & population);
+
+void genocide(GeneticPopulation & population, std::vector<GeneticIndividual> survivors, const Problem & problem);
+
+void printIndividual(GeneticIndividual ind);
+
+void evolve(GeneticPopulation & population, const Problem & problem);
 
 void solveGenetic(const Problem & problem);
